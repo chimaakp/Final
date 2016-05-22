5beba01919d3ae9cea8f8b01adae1b4a95eb9116
chimaakp <chimaakp@ChimaII>
2016-05-22 00:30:50 -0400

    ugiyf

--- RocketBLL/src/test/java/rocketBase/rate_test.java
+++ RocketBLL/src/test/java/rocketBase/rate_test.java
@@ -4,6 +4,7 @@ import static org.junit.Assert.*;
 
 import org.junit.Test;
 
+import rocketBase.RateBLL;
 public class rate_test {
 
 	//TODO - RocketBLL rate_test
@@ -13,8 +14,17 @@ public class rate_test {
 	//		Check to see if a RateException is thrown if there are no rates for a given
 	//		credit score
 	@Test
-	public void test() {
-		assert(1==1);
+	public void rate_test(){
+		double t1;
+		try{
+			
+		
+		t1= RateBLL.getRate(700);}
+		
+		catch(Exception e){
+			
+		}
+		assertTrue( t1 == 4);
 	}
 
 }
